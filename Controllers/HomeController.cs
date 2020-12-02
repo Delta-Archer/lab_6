e76fd0666b562457acdc40be315b9401ff602239
Delta-Archer <71624858+Delta-Archer@users.noreply.github.com>
2020-12-02 14:01:02 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Lab_6.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
